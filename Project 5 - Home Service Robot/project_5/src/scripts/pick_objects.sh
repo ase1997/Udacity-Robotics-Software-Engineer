d39c7c88703785516e87743ce9002116349f917a
ase1997 <kdo16197@gmail.com>
2022-09-07 18:12:12 -0700

    project_5

---
+++ Project 5 - Home Service Robot/project_5/src/scripts/pick_objects.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(rospack find worlds)/khoa.world" &
+sleep 5
+
+xterm -e " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$(rospack find maps)/map.yaml" &
+sleep 5
+
+xterm -e " roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+sleep 5
+
+xterm -e " rosrun pick_objects pick_objects"
